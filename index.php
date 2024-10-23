9ebe6a5d2d95ab634a25d038065167c9b5dd7422
muh-younus <braq369@gmail.com>
2024-10-23 16:30:32 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Facebook Form</title>
+</head>
+<body>
+
+
+    <div class="container-fluid cont1 ">
+
+    <h3>facebook</h3>
+
+    <div class="main">
+
+    
+
+    <div class="row">
+
+    <div class="col-lg-12">
+
+    <form action="">
+
+    <p>Login in to Facebook</p>
+
+    <input type="text" name="name" placeholder="Email address or phone number">
+    <input type="text" name="password" placeholder="Password">
+
+    <button>Log in</button>
+
+    <div class="anker">
+
+    <a href="">Forgotten account?</a>
+    <a href="">Sign up For Facebook</a>
+    </div>
+    </form>
+
+
+    
+
+
+    </div>
+
+    </div>
+
+
+
+    </div>
+
+
+
+
+
+    </div>
+</body>
+</html>
