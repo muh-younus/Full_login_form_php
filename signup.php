69b6a8b68b3f4efda4413f171b2bb55ea21edb3f
muh-younus <braq369@gmail.com>
2024-10-26 02:24:35 +0500

    thired Commit

--- signup.php
+++ signup.php
@@ -22,7 +22,40 @@
 <input type="text">
 <input type="text">
 <p>Date of birth</p>
-<input type="checkbox" name="days[]" value="7">
+<select name="day">
+    <option value="1" selected>1</option>
+    <option value="2" selected>2</option>
+    <option value="3" selected>3</option>
+    <option value="4" selected>4</option>
+    <option value="5" selected>5</option>
+    <!-- Other options for days -->
+  </select>
+
+  <select name="month">
+    <option value="Oct" selected>Jan</option>
+    <option value="Oct" selected>Feb</option>
+    <option value="Oct" selected>March</option>
+    <option value="Oct" selected>April</option>
+    <option value="Oct" selected>May</option>
+    <!-- Other options for months -->
+  </select>
+
+  <select name="year">
+    <option value="2024" selected>2024</option>
+    <option value="2025" selected>2025</option>
+    <option value="2026" selected>2026</option>
+    <option value="2027" selected>2027</option>
+    <option value="2028" selected>2028</option>
+    <!-- Other options for years -->
+  </select>
+
+  <p>Gender</p>
+  <label>
+    <input type="radio" name="gender" value="female">
+    Female
+  </label>
+
+
 </form>
 
 
